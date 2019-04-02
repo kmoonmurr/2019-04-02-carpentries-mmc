15b283d5160608e624e7beead3a6d7aabd9163df
kmoonmurr <kim@pmurray.com>
2019-04-02 17:06:46 +0000

    Summarie gapminder and create plot of life expectancy

--- src/data/Gapminder/get_gapminder_data.R
+++ src/data/Gapminder/get_gapminder_data.R
@@ -2,3 +2,7 @@
 
 library(here)
 file_url <- "https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv"
+
+download.file(url=file_url, destfile=here("data/gapminder/raw/gapminder_data.csv"))
+
+
